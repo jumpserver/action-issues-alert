4f83bf8dfe9095b7771e4ba81bd8062b01424866
ibuler <ibuler@qq.com>
2021-12-03 18:51:27 +0800

    fix: 修复时间错误

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ WEB_HOOK=${INPUT_HOOK}
 REPO=${INPUT_REPO-$GITHUB_REPOSITORY}
 TYPE=${INPUT_TYPE-all}
 INACTIVE_DAY=${INPUT_INACTIVE-30}
-RECENT_DAY=${INPUT_INACTIVE-2}
+RECENT_DAY=${INPUT_RECENT-2}
 
 python /check_inactive_issues.py "${REPO}" --hook "${WEB_HOOK}" --type "${TYPE}" \
     --inactive "${INACTIVE_DAY}" --recent "${RECENT_DAY}"
